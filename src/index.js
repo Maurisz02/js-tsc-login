f1ef322cb783bb0140d7cda722fad0d7bf93c999
Maurisz <p2002adam@gmail.com>
2022-10-12 14:53:46 +0200

    e-mail + password working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 "use strict";
+let jelszo = "";
 document.addEventListener('DOMContentLoaded', () => {
-    var _a;
+    var _a, _b, _c, _d;
     (_a = document.getElementById('username')) === null || _a === void 0 ? void 0 : _a.addEventListener('change', (e) => {
         let name = e.currentTarget;
         let label = document.getElementById('userCheck');
@@ -16,6 +17,48 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         else {
             name.style.color = "White";
+            label.textContent = "";
+        }
+    });
+    (_b = document.getElementById('email')) === null || _b === void 0 ? void 0 : _b.addEventListener('change', (e) => {
+        let email = e.currentTarget;
+        let label = document.getElementById('emailCheck');
+        const valid = (emailC) => {
+            return String(emailC)
+                .toLowerCase()
+                .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        };
+        if (!valid(email.value)) {
+            label.textContent = "Helytelen e-mail formátum";
+            label.style.color = "Red";
+            email.style.color = "Red";
+        }
+        else {
+            email.style.color = "White";
+            label.textContent = "";
+        }
+    });
+    (_c = document.getElementById('passwd')) === null || _c === void 0 ? void 0 : _c.addEventListener('change', (e) => {
+        let passwd = e.currentTarget;
+        let label = document.getElementById('passwdCheck');
+        var passwdC = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
+        if (!passwd.value.match(passwdC)) {
+            label.textContent = "A jelszónak 8-10 karakter, tartalmaznia kell kis és nagy betűt, számot és speciális karaktert";
+            label.style.color = "Red";
+        }
+        else {
+            label.textContent = "";
+            jelszo = passwd.value;
+        }
+    });
+    (_d = document.getElementById('rePasswd')) === null || _d === void 0 ? void 0 : _d.addEventListener('change', (e) => {
+        let rePasswd = e.currentTarget;
+        let label = document.getElementById('rePasswdCheck');
+        if (rePasswd.value != jelszo) {
+            label.textContent = "A két jelszó nem egyezik";
+            label.style.color = "Red";
+        }
+        else {
             label.textContent = "";
         }
     });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 
+let jelszo : string = "";
 
 document.addEventListener('DOMContentLoaded', ()=>{
 
@@ -19,6 +20,50 @@ document.addEventListener('DOMContentLoaded', ()=>{
         }
     });
 
+    document.getElementById('email')?.addEventListener('change', (e)=>{
+        let email = e.currentTarget as HTMLInputElement;
+        let label = (document.getElementById('emailCheck') as HTMLLabelElement);
+        const valid = (emailC : string) => {
+            return String(emailC)
+              .toLowerCase()
+              .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+              );
+          };
 
+          if(!valid(email.value)){
+            label.textContent = "Helytelen e-mail formátum";
+            label.style.color = "Red";
+            email.style.color = "Red";
+          }else{
+            email.style.color = "White";
+            label.textContent = "";
+          }
+    });
+
+    document.getElementById('passwd')?.addEventListener('change', (e)=>{
+        let passwd = e.currentTarget as HTMLInputElement;
+        let label = (document.getElementById('passwdCheck') as HTMLLabelElement);
+        var passwdC=  /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
+        if(!passwd.value.match(passwdC)){
+            label.textContent = "A jelszónak 8-10 karakter, tartalmaznia kell kis és nagy betűt, számot és speciális karaktert"
+            label.style.color = "Red";
+        }else{
+            label.textContent = "";
+            jelszo = passwd.value;
+        }
+    });
+
+    document.getElementById('rePasswd')?.addEventListener('change',(e)=>{
+        let rePasswd = e.currentTarget as HTMLInputElement;
+        let label = (document.getElementById('rePasswdCheck') as HTMLLabelElement);
+
+        if(rePasswd.value != jelszo){
+            label.textContent = "A két jelszó nem egyezik"
+            label.style.color = "Red";
+        }else{
+            label.textContent = "";
+        }
+    });
 
 });
