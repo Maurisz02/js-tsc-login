43eb5aec4d93767ea6ca545ed00ab68274d588a8
Maurisz <p2002adam@gmail.com>
2022-10-12 14:19:23 +0200

    username + chechk is done

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ document.addEventListener('DOMContentLoaded', () => {
     var _a;
     (_a = document.getElementById('username')) === null || _a === void 0 ? void 0 : _a.addEventListener('change', (e) => {
         let name = e.currentTarget;
-        console.log(name.value);
+        let label = document.getElementById('userCheck');
+        if (name.value.includes("&") || name.value.includes("=") || name.value.includes("_") || name.value.includes("'") || name.value.includes("+") || name.value.includes("-") || name.value.includes(",") || name.value.includes(">") || name.value.includes("<")) {
+            label.textContent = "Felhasználónév nem tartalmazhat bizonyos karaktereket";
+            label.style.color = "Red";
+            name.style.color = "Red";
+        }
+        else if (name.value.length < 6 || name.value.length > 30) {
+            label.textContent = "A nevednek minimum 6 karakternek kell lennie és maximum 30 karaktert tartalmazhat";
+            label.style.color = "Red";
+            name.style.color = "Red";
+        }
+        else {
+            name.style.color = "White";
+            label.textContent = "";
+        }
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,21 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     document.getElementById('username')?.addEventListener('change',(e) : void=>{
         let name = e.currentTarget as HTMLInputElement;
-        
-        console.log(name.value);
+        let label = (document.getElementById('userCheck') as HTMLLabelElement);
+        if(name.value.includes("&") || name.value.includes("=") || name.value.includes("_") || name.value.includes("'") || name.value.includes("+") || name.value.includes("-") || name.value.includes(",") || name.value.includes(">") || name.value.includes("<")){
+            label.textContent = "Felhasználónév nem tartalmazhat bizonyos karaktereket"
+            label.style.color = "Red";
+            name.style.color = "Red";
+        }else if(name.value.length < 6 || name.value.length > 30){
+            label.textContent = "A nevednek minimum 6 karakternek kell lennie és maximum 30 karaktert tartalmazhat"
+            label.style.color = "Red";
+            name.style.color = "Red";
+        }else{
+            name.style.color = "White";
+            label.textContent = "";
+        }
     });
+
+
 
 });
